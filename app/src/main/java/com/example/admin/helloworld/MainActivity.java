9728b7b54ede5d5ad42945ea21fc6164de66fbec
Chris Volkernick <cvolkern@gmail.com>
2018-03-21 18:33:12 -0400

    more text, image, button / toast

--- app/src/main/java/com/example/admin/helloworld/MainActivity.java
+++ app/src/main/java/com/example/admin/helloworld/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.admin.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Toast.makeText(getApplication(),"Mmm...Toasty.",3).show();
     }
 }
